509c4c8e012d006284566933a5de73db0826fba7
Machi Hoshino <mhoshino@mirantis.com>
2018-10-23 16:38:22 +0900

    Update

--- create_speedsheet.py
+++ create_speedsheet.py
@@ -6,7 +6,7 @@ from openpyxl.styles import PatternFill, Border, Side, Font
 import dateutil.parser
 from pytz import timezone
 import pytz
-import datetime
+from datetime import datetime, timedelta
 
 ### Personal Credentials
 parser = SafeConfigParser()
@@ -20,6 +20,7 @@ sf_cloud = parser.get('SalesForce', 'cloud')
 last_time = parser.get('SalesForce', 'last_time')
 now = parser.get('SalesForce', 'now')
 tz  = parser.get('SalesForce', 'tz')
+duration = int(parser.get('SalesForce', 'duration'))
 report_file_name  = parser.get('SalesForce', 'report_file_name')
 
 ### L1 Variables
@@ -114,6 +115,7 @@ summary_logic = [
         "name" : "Total Cases",
         "logic" : "COUNT()",
         "result" : "totalSize",
+        "duration": 0,
         "base_query" : [
            "AccountId = '%s'" % sf_account,
            "Environment2__c = '%s'" % sf_cloud,
@@ -124,191 +126,257 @@ summary_logic = [
            "CreatedDate < "
         ]
     },
-    {
-        "name" :"Cases Opened By Customer",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
-           "( CreatedById = '%s' )" % "' Or CreatedById = '".join(customer_ids)
-        ],
-        "time_query": [
-           "CreatedDate < "
-        ]
-    },
-    {
-        "name" : "Opened Sev1 Cases",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
-           "Severity_Level__c = 'Sev 1'"
-        ],
-        "time_query": [
-           "CreatedDate < "
-        ]
-    },
-    {
-        "name" : "Opened Sev2 Cases",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
-           "Severity_Level__c = 'Sev 2'"
-        ],
-        "time_query": [
-           "CreatedDate < "
-        ]
-    },
-    {
-        "name" : "Opened Sev3 Cases",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
-           "Severity_Level__c = 'Sev 3'"
-        ],
-        "time_query": [
-           "CreatedDate < "
-        ]
-    },
-    {
-        "name" : "Opened Sev4 Cases",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
-           "Severity_Level__c = 'Sev 4'"
-        ],
-        "time_query": [
-           "CreatedDate < "
-        ]
-    },
-    {
-        "name" : "Technical Escalated Cases",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "L2__c = true",
-           "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
-        ],
-        "time_query": [
-           "CreatedDate < "
-        ]
-    },
-    {
-        "name" : "Closed Cases(Including Merged Cases)",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
-           "ClosedDate != null",
-        ],
-        "time_query": [
-           "ClosedDate < "
-        ]
-    },
-    {
-       "name": "Average resolution time Sev 2 (in hours)",
-       "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
-       "result" : "aver",
+   {
+       "name" : "Opened Sev1 Cases",
+       "logic" : "COUNT()",
+       "result" : "totalSize",
+       "duration": 0,
        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "RecordTypeId = '%s' " % tech_case_id,
-           "Severity_Level__c = 'Sev 2'",
-           "ClosedDate != null",
-        ],
-        "time_query": [
-           "ClosedDate < "
-        ]
-    },
-    {
-       "name": "Average resolution time Sev 3 (in hours)",
-       "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
-       "result" : "aver",
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
+          "Severity_Level__c = 'Sev 1'"
+       ],
+       "time_query": [
+          "CreatedDate < "
+       ]
+   },
+   {
+       "name" : "Opened Sev2 Cases",
+       "logic" : "COUNT()",
+       "result" : "totalSize",
+       "duration": 0,
        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "RecordTypeId = '%s' " % tech_case_id,
-           "Severity_Level__c = 'Sev 3'",
-           "ClosedDate != null",
-        ],
-        "time_query": [
-           "ClosedDate < "
-        ]
-    },
-    {
-       "name": "Average resolution time Sev 4 (in hours)",
-       "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
-       "result" : "aver",
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
+          "Severity_Level__c = 'Sev 2'"
+       ],
+       "time_query": [
+          "CreatedDate < "
+       ]
+   },
+   {
+       "name" : "Opened Sev3 Cases",
+       "logic" : "COUNT()",
+       "result" : "totalSize",
+       "duration": 0,
        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "RecordTypeId = '%s' " % tech_case_id,
-           "Severity_Level__c = 'Sev 4'",
-           "ClosedDate != null",
-        ],
-        "time_query": [
-           "ClosedDate < "
-        ]
-    },
-    {
-        "name" : "Completed Change Requests",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "RecordTypeId = '%s'" % change_request_id,
-           "ClosedDate != null",
-           "status != 'Canceled'"
-        ],
-        "time_query": [
-           "CreatedDate < "
-        ]
-    },
-    {
-        "name" : "Opened Change Requests",
-        "logic" : "COUNT()",
-        "result" : "totalSize",
-        "base_query" : [
-           "AccountId = '%s'" % sf_account,
-           "Environment2__c = '%s'" % sf_cloud,
-           "isMosAlert__c = false",
-           "RecordTypeId = '%s'" % change_request_id,
-           "ClosedDate = null",
-           "status != 'Canceled'"
-        ],
-        "time_query": [
-           "CreatedDate < "
-        ]
-    }
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
+          "Severity_Level__c = 'Sev 3'"
+       ],
+       "time_query": [
+          "CreatedDate < "
+       ]
+   },
+   {
+       "name" : "Opened Sev4 Cases",
+       "logic" : "COUNT()",
+       "result" : "totalSize",
+       "duration": 0,
+       "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
+          "Severity_Level__c = 'Sev 4'"
+       ],
+       "time_query": [
+          "CreatedDate < "
+       ]
+   },
+   {
+       "name" : "Closed Cases",
+       "logic" : "COUNT()",
+       "result" : "totalSize",
+       "duration": 0,
+       "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
+          "ClosedDate != null",
+       ],
+       "time_query": [
+          "ClosedDate < "
+       ]
+   },
+   {
+       "name" :"Cases Opened By Customer",
+       "logic" : "COUNT()",
+       "result" : "totalSize",
+       "duration": 0,
+       "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
+          "( CreatedById = '%s' )" % "' Or CreatedById = '".join(customer_ids)
+       ],
+       "time_query": [
+          "CreatedDate < "
+       ]
+   },
+   {
+       "name" : "Technical Escalated Cases",
+       "logic" : "COUNT()",
+       "result" : "totalSize",
+       "duration": 0,
+       "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "L2__c = true",
+          "( RecordTypeId = '%s' or RecordTypeId = '%s' )" % ( tech_case_id, closed_case_id),
+       ],
+       "time_query": [
+          "CreatedDate < "
+       ]
+   },
+   {
+      "name": "%s Days Average resolution time Sev 2 (in hours)" % duration,
+      "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
+      "result" : "aver",
+      "duration": duration,
+      "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "RecordTypeId = '%s' " % tech_case_id,
+          "Severity_Level__c = 'Sev 2'",
+          "ClosedDate != null",
+       ],
+       "time_query": [
+          "ClosedDate < ",
+          "ClosedDate > "
+       ]
+   },
+   {
+      "name": "%s Days Average resolution time Sev 3 (in hours)" % duration,
+      "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
+      "result" : "aver",
+      "duration": duration,
+      "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "RecordTypeId = '%s' " % tech_case_id,
+          "Severity_Level__c = 'Sev 3'",
+          "ClosedDate != null",
+       ],
+       "time_query": [
+          "ClosedDate < ",
+          "ClosedDate > "
+       ]
+   },
+   {
+      "name": "%s Days Average resolution time Sev 4 (in hours)" % duration,
+      "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
+      "result" : "aver",
+      "duration": duration,
+      "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "RecordTypeId = '%s' " % tech_case_id,
+          "Severity_Level__c = 'Sev 4'",
+          "ClosedDate != null",
+       ],
+       "time_query": [
+          "ClosedDate < ",
+          "ClosedDate > "
+       ]
+   },
+   {
+      "name": "All Time Average resolution time Sev 2 (in hours)",
+      "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
+      "result" : "aver",
+      "duration": 0,
+      "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "RecordTypeId = '%s' " % tech_case_id,
+          "Severity_Level__c = 'Sev 2'",
+          "ClosedDate != null",
+       ],
+       "time_query": [
+          "ClosedDate < "
+       ]
+   },
+   {
+      "name": "All Time Average resolution time Sev 3 (in hours)",
+      "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
+      "result" : "aver",
+      "duration": 0,
+      "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "RecordTypeId = '%s' " % tech_case_id,
+          "Severity_Level__c = 'Sev 3'",
+          "ClosedDate != null",
+       ],
+       "time_query": [
+          "ClosedDate < "
+       ]
+   },
+   {
+      "name": "All Time Average resolution time Sev 4 (in hours)",
+      "logic" : "AVG(Customer_Wait_Time_in_Hours__c)aver",
+      "result" : "aver",
+      "duration": 0,
+      "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "RecordTypeId = '%s' " % tech_case_id,
+          "Severity_Level__c = 'Sev 4'",
+          "ClosedDate != null",
+       ],
+       "time_query": [
+          "ClosedDate < "
+       ]
+   },
+   {
+       "name" : "Completed Change Requests",
+       "logic" : "COUNT()",
+       "result" : "totalSize",
+       "duration": 0,
+       "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "RecordTypeId = '%s'" % change_request_id,
+          "ClosedDate != null",
+          "status != 'Canceled'"
+       ],
+       "time_query": [
+          "CreatedDate < "
+       ]
+   },
+   {
+       "name" : "Opened Change Requests",
+       "logic" : "COUNT()",
+       "duration": 0,
+       "result" : "totalSize",
+       "base_query" : [
+          "AccountId = '%s'" % sf_account,
+          "Environment2__c = '%s'" % sf_cloud,
+          "isMosAlert__c = false",
+          "RecordTypeId = '%s'" % change_request_id,
+          "ClosedDate = null",
+          "status != 'Canceled'"
+       ],
+       "time_query": [
+          "CreatedDate < "
+       ]
+   }
 
 ]
 summary_page = [
@@ -326,6 +394,7 @@ solved_tech_cases = [
         #"SLA Violated",
         "Resolution time",
         "Created By Customer",
+        "Change Request",
         "Created",
         "Closed",
         "Subject"
@@ -337,8 +406,10 @@ open_tech_cases = [
         "Status",
         "Severity",
         #"SLA Violated",
+        "Resolution time",
         "Created By Customer",
         "Escalated",
+        "Change Request",
         "Created",
         "Subject"
     ]
@@ -382,11 +453,16 @@ for i in range(len(summary_logic)):
     summary=summary_logic[i]["name"]
     row=[summary]
     for date in [ now, last_time ]:
-      query_string = "SELECT %s from Case where %s and %s %s" % (
+      time_array=[ "%s %s" % (summary_logic[i]["time_query"][0],date) ] 
+      if summary_logic[i]["duration"] > 0:
+         duration_date=dateutil.parser.parse(date) - timedelta(days=summary_logic[i]["duration"])
+         time_array.append("%s %s" % (summary_logic[i]["time_query"][1],
+             duration_date.strftime('%Y-%m-%dT%H:%M:%S%z') )) 
+      query_string = "SELECT %s from Case where %s and %s" % (
           summary_logic[i]["logic"] ,
           ' and '.join(summary_logic[i]["base_query"]) ,
-          ' and '.join(summary_logic[i]["time_query"]),
-          date)
+          ' and '.join(time_array)
+          )
       if summary_logic[i]["result"] == "totalSize":
          row.append( sf.query(query_string)['totalSize'])
       else:
@@ -407,7 +483,7 @@ for case in cases:
                     isCustomerCreated = False
                 row = [ case['CaseNumber'], case['Severity_Level__c'], #case['Resolution_Time_is_violated__c'], 
                         case['Customer_Wait_Time_in_Hours__c'],
-                        isCustomerCreated, case['CreatedDate'], case['ClosedDate'], case['Subject'] ]
+                        isCustomerCreated, "", case['CreatedDate'], case['ClosedDate'], case['Subject'] ]
                 solved_tech_cases.append(row)
                 #if case['Resolution_Time_is_violated__c'] == True:
                 #    row = [ case['CaseNumber'], case['Severity_Level__c'], isCustomerCreated, case['L2__c'], case['CreatedDate'], case['ClosedDate'], # case['Resolution_Time_DDHHMM__c'],
@@ -419,7 +495,8 @@ for case in cases:
                 else:
                     isCustomerCreated = False
                 row = [ case['CaseNumber'], case['Status'], case['Severity_Level__c'], #case['Resolution_Time_is_violated__c'], 
-                        isCustomerCreated, case['L2__c'], case['CreatedDate'], case['Subject'] ]
+                        case['Customer_Wait_Time_in_Hours__c'],
+                        isCustomerCreated, case['L2__c'], "", case['CreatedDate'], case['Subject'] ]
                 open_tech_cases.append(row)
             if record_type["Name"] == "Change Request" and case['Status'] == 'Completed':
                 maint_start = case['CreatedDate']
